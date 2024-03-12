e39e1ea425d178bee0aa6adf6353acd3160239ca
shagunsharma6677 <shagunsharma6677@gmail.com>
2024-03-12 23:50:12 +0530

    feat: Added shadcn workspace in TurboRepo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import { Button } from "@repo/ui/button";
+import { Button } from "@repo/ui";
 
 export default function Page(): JSX.Element {
   return (
     <main className="bg-slate-900 flex items-center justify-center h-screen text-4xl text-cyan-50">
-      This is Home Page of Dischord app !!!
+      <Button variant={"destructive"}>Shadcn-ui Button</Button>
     </main>
   );
 }
