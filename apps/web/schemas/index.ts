1eefd38d15c11634eb48ccb4af7abc6c5cf90d44
shagunsharma6677 <shagunsharma6677@gmail.com>
2024-03-17 18:01:40 +0530

    add: Created validation schema from zod

--- apps/web/schemas/index.ts
+++ apps/web/schemas/index.ts
@@ -1,6 +1,16 @@
-import {z} from "zod";
+import { z } from "zod";
 
 export const LoginSchema = z.object({
   email: z.string().email(),
   password: z.string(),
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6, {
+    message: "Minimum 6 characters required !",
+  }),
+  name: z.string().min(1, {
+    message: "Name is required !",
+  }),
 });
