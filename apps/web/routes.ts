9fffae8050721785a4ab3d3c1d53a4e93ab89e1b
shagunsharma6677 <shagunsharma6677@gmail.com>
2024-03-17 17:58:06 +0530

    add: Created route.ts for end-point middleware validation.

---
+++ apps/web/routes.ts
@@ -0,0 +1,25 @@
+/**
+ * An Array of routes that are accessible to the public.
+ * These routes do not require authentication.
+ * @type {string[]}
+ */
+export const publicRoutes: string[] = ["/"];
+
+/**
+ * An Array of routes that are used for authentication.
+ * These routes will redirect logged-in users to /[protected-routes]
+ * @type {string[]}
+ */
+export const authRoutes: string[] = ["/auth/login", "/auth/register"];
+
+/**
+ * Prefix for authentication API routes
+ * This will always be allowed by both loggedIn and logged out user.
+ * @type {string[]}*/
+export const apiAuthPrefix: string = "/api/auth";
+
+/**
+ * Default redirect path after loggingIn.
+ * @type {string[]}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/"
