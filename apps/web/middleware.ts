763dc200601c52bd843b00d1708f4128dd91eb3e
shagunsharma6677 <shagunsharma6677@gmail.com>
2024-03-25 22:57:53 +0530

    Update: Update middleware for authjs

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,40 +1,47 @@
-import { NextRequest, NextResponse } from "next/server";
+
 import {
   DEFAULT_LOGIN_REDIRECT,
   apiAuthPrefix,
   publicRoutes,
   authRoutes,
 } from "./routes";
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
 
-export default async function middleware(req: any) {
+const { auth }: any = NextAuth(authConfig);
+
+export default auth((req:any) => {
+  // req.auth
   const { nextUrl } = req;
-  const isLoggedIn = false;
+  const isLoggedIn = !!req.auth;
   console.log("ROUTE", req.nextUrl.pathname);
   console.log("isLoggedIn", isLoggedIn);
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  //If user is already loggedIn we will not allow them to access login page.
+  //If user loggedout then dont allow them to acccess setting [protected routes page]
 
   if (isApiAuthRoute) {
     return null;
   }
 
+  // By default we want user to allow auth route
   if (isAuthRoute) {
+    //But before we check whather user is loggedIn or not
     if (isLoggedIn) {
-      return NextResponse.redirect(
-        new URL(DEFAULT_LOGIN_REDIRECT, nextUrl).toString()
-      );
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
     return null;
   }
 
   if (!isLoggedIn && !isPublicRoute) {
-    return NextResponse.redirect(new URL("/auth/login", nextUrl).toString());
+    return Response.redirect(new URL("/auth/login", nextUrl));
   }
 
   return null;
-}
+});
 // Optionally, don't invoke Middleware on some paths
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
