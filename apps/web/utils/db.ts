5a56532b0ad1b6d5a69c98d8f01b44454e3c12bc
shagunsharma6677 <shagunsharma6677@gmail.com>
2024-03-25 22:54:37 +0530

    Update: Update db.ts

--- apps/web/utils/db.ts
+++ apps/web/utils/db.ts
@@ -1,33 +1,31 @@
-import mongoose from "mongoose";
+// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
+import { MongoClient } from "mongodb"
 
-const MONGO_URI = String(process.env.MONGO_URI);
-const cached: {
-  connection?: typeof mongoose;
-  promise?: Promise<typeof mongoose>;
-} = {};
-async function connectMongo() {
-  if (!MONGO_URI) {
-    throw new Error(
-      "Please define the MONGO_URI environment variable inside .env.local"
-    );
-  }
-  if (cached.connection) {
-    return cached.connection;
-  }
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-    cached.promise = mongoose.connect(MONGO_URI, opts);
-  }
-  try {
-    cached.connection = await cached.promise;
-  } catch (e) {
-    cached.promise = undefined;
-    throw e;
-  }
-
-  console.log("mongoDB conncted");
-  return cached.connection;
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
-export default connectMongo;
+
+const uri = process.env.MONGODB_URI
+const options = {}
+
+let client
+let clientPromise: Promise<MongoClient>
+
+if (process.env.NODE_ENV == "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  // console.log("insideDevEnv")
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options)
+    global._mongoClientPromise = client.connect()
+  }
+  clientPromise = global._mongoClientPromise
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options)
+  clientPromise = client.connect()
+}
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise
