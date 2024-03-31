bcb93ed88cf346ac702ba27fc6979139be06082f
shagunsharma6677 <shagunsharma6677@gmail.com>
2024-03-31 16:56:48 +0530

    add: added next auth credential provider

--- apps/web/auth.config.ts
+++ apps/web/auth.config.ts
@@ -1,6 +1,29 @@
-import Github from "next-auth/providers/github";
 import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "./schemas";
+import { findByEmail } from "./lib/utils";
+import bcrypt from "bcryptjs";
 
 export default {
-  providers: [Github],
+  providers: [
+    Credentials({
+      name:"credentials",
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await findByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -1,16 +1,22 @@
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "./utils/db";
 import authConfig from "./auth.config";
-import NextAuth, { NextAuthConfig } from "next-auth";
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "./lib/db";
+
 
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
+  signOut,
 }: any = NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
-  session: {
-    // Set it as jwt instead of database
-    strategy: "jwt",
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/login",
   },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  secret: process.env.AUTH_SECRET,
+  debug: process.env.NODE_ENV === "development",
   ...authConfig,
 });
