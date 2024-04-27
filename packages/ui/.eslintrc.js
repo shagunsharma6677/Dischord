9c28436655e430e3e79b5b9145e79938540de4e6
shagunsharma6677 <shagunsharma6677@gmail.com>
2024-04-27 13:48:53 +0530

    feat(shadcn/ui): added shadcn/ui components

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   root: true,
   extends: ["@repo/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.lint.json",
-  },
+  // parserOptions: {
+  //   project: "./tsconfig.lint.json",
+  // },
 };
--- packages/ui/postcss.config.js
+++ packages/ui/postcss.config.js
@@ -1,6 +1,8 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
 
+/* eslint-env node */
+
 module.exports = {
   plugins: {
     tailwindcss: {},
