e7d820e9edb269d69b7a2d0a8ba4545d928cbe1a
shagunsharma6677 <shagunsharma6677@gmail.com>
2024-05-08 00:06:20 +0530

    ci: react vite and next-ui setup

---
+++ apps/react-client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+// import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <div className="bg-black h-screen">lorem</div>
+    </Fragment>
+  );
+}
+
+export default App;
