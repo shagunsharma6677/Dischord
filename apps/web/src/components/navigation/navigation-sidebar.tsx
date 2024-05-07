1acb54b681ce6292d57a1fca7f944644e1d7aa14
shagunsharma6677 <shagunsharma6677@gmail.com>
2024-05-07 15:14:08 +0530

    feat(component): update and add new components

--- apps/web/src/components/navigation/navigation-sidebar.tsx
+++ apps/web/src/components/navigation/navigation-sidebar.tsx
@@ -29,10 +29,10 @@ export const NavigationSidebar = async () => {
   });
 
   return (
-    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
+    <div className="border border-blue-500  space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
       <NavigationAction />
       <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
-      <ScrollArea className="flex-1 w-full">
+      <ScrollArea className="flex-1 w-full border border-green-600">
         {servers.map((server) => (
           <div key={server.id} className="mb-4">
             <NavigationItem
@@ -43,7 +43,7 @@ export const NavigationSidebar = async () => {
           </div>
         ))}
       </ScrollArea>
-      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+      <div className="flex items-center flex-col pb-3 mt-auto gap-y-4">
         <ModeToggle />
         <UserButton />
       </div>
