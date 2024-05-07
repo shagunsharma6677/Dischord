63bb6d6a848506847e17c11177ce4877a5675661
shagunsharma6677 <shagunsharma6677@gmail.com>
2024-05-07 15:18:36 +0530

    feat(provider): completed provider module

--- apps/web/src/components/providers/ModalProvider.tsx
+++ apps/web/src/components/providers/ModalProvider.tsx
@@ -7,6 +7,9 @@ import { MembersModal } from '../modals/members-modal';
 import { CreateChannelModal } from '../modals/create-channel-modal';
 import { LeaveServerModal } from '../modals/leave-server-modal';
 import { DeleteServerModal } from '../modals/delete-server-modal';
+import { EditChannelModal } from '../modals/edit-channel-modal';
+import { MessageFileModal } from '../modals/message-file-modal';
+import { DeleteMessageModal } from '../modals/delete-message-modal';
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -28,6 +31,9 @@ export const ModalProvider = () => {
       <CreateChannelModal />
       <LeaveServerModal />
       <DeleteServerModal />
+      <EditChannelModal />
+      <MessageFileModal />
+      <DeleteMessageModal />
     </>
   );
 };
