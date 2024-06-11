81493596854c9c115ff022d02dba1e8e3c191f01
shagunsharma6677 <shagunsharma6677@gmail.com>
2024-06-11 21:21:40 +0530

    feat(createServer): added createServer util

---
+++ apps/server/utils/create-server.js
@@ -0,0 +1,45 @@
+import http from 'http';
+import https from 'https';
+import { initializeDatabase } from './db.js';
+import { log } from './logger.js';
+
+const createServer = async (app, port = 5030) => {
+  let server;
+
+  try {
+    // Attempt to start HTTPS
+    server = https.createServer(app);
+    server.listen(port, async () => {
+      try {
+        await initializeDatabase();
+        log('[2] Connected to Prisma Database with https');
+      } catch (err) {
+        console.error('Failed to connect to Prisma Database:', err);
+        process.exit(1); // Terminate process on Prisma connection error
+      }
+    });
+    server.timeout = 18000;
+  } catch (httpsError) {
+    console.error('Failed to start HTTPS server:', httpsError);
+    // Attempt to start HTTP server if HTTPS fails
+    try {
+      server = http.createServer(app);
+      server.listen(port, async () => {
+        try {
+          await initializeDatabase();
+          log('[2] Connected to Prisma Database with http');
+        } catch (err) {
+          console.error('Failed to connect to Prisma Database:', err);
+        }
+      });
+      server.timeout = 18000;
+    } catch (httpError) {
+      console.error('Failed to start HTTP server:', httpError);
+      process.exit(1); // Terminate process on server startup error
+    }
+  }
+
+  return server;
+};
+
+export default createServer;
